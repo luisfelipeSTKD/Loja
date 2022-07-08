52a4796a46563cb7cc0bfa8ebf5df90ed5e48fa9
Luis Felipe STKD <70490891+luisfelipeSTKD@users.noreply.github.com>
2022-07-08 20:20:05 -0300

    mudança de rotinas e funções

--- teste.py
+++ teste.py
@@ -35,27 +35,35 @@ def cadastroCliente():
 
 nomeCliente, anoAtual, anoNascimento, salario = cadastroCliente()
 
-# Coloque o código que você fez nas etapas 1 e 2 dentro de uma única função chamada “obter_limite”. Essa função deverá retornar o limite que o usuário poderá gastar.
-
 def obter_limite():
 
     idade = anoAtual - anoNascimento
     limiteDeGasto = (salario * (idade/1000)) + 100
 
+    print(f"O seu limite lirado é a quantia de R${limiteDeGasto}\n")
+
+    print("Análise finalizada!\n")
+
     return limiteDeGasto
 
 limiteDeGasto = obter_limite()
 
-print(f"O seu limite lirado é a quantia de R${limiteDeGasto}\n")
-
-print("Análise finalizada!\n")
-
-# Coloque o código que você fez na etapa 3 dentro de uma única função chamada “verificar_produto”. Essa função terá como parâmetro de entrada o limite de gasto do cliente.
-
 def verificar_produto(limiteDeGasto):
 
+    if totalProdutos in produtosDisponiveis:
+        qtdProdutos = int(input("Quantos"))
+
+    totalProdutos = 0
+    # limiteMax = 0
+    while totalProdutos < limiteDeGasto:
+        produtosCadastrados = input("Informe os produtos para cadastrar: ")
+        print(f"\nProdutos cadastrados {totalProdutos + 1}")
+
+        # Dentro da sua estrutura de repetição, chame sua função “verificar_produto”, permitindo que o usuário consiga cadastrar todos os produtos e verificar se teria ou não limite sobrando para comprá-los.
+
+        totalProdutos, limiteMax = verificar_produto(limiteDeGasto)
     # limite = limiteDeGasto
-    produto = input("Qual produto gostaria de comprar? ")
+    # produto = input("Qual produto gostaria de comprar? ")
     precoProduto = float(input("\nE o valor dele em reais: "))
 
     limite60 = float(precoProduto * 0.60)
@@ -72,24 +80,22 @@ def verificar_produto(limiteDeGasto):
 
 produto, precoProduto = verificar_produto(limiteDeGasto)
 
+# Coloque o código que você fez nas etapas 1 e 2 dentro de uma única função chamada “obter_limite”. Essa função deverá retornar o limite que o usuário poderá gastar.
+
+
+
+
+# Coloque o código que você fez na etapa 3 dentro de uma única função chamada “verificar_produto”. Essa função terá como parâmetro de entrada o limite de gasto do cliente.
+
+
 # Após o cliente informar os dados dele (pela função “obter_limite”), armazene o limite que ele poderá gastar dentro de uma variável chamada “limite”.
 
 limite = obter_limite()
 
-print(limite)
+# print(limite)
 
 # Por fim, utilize uma estrutura de repetição (for ou while) por n vezes, com n equivalendo à quantidade de produtos que ele deseja cadastrar.
 
 produtosDisponiveis = ['Mesa', 'Cadeira', 'Pia']
 
 print("Quais produtos deseja cadastrar? ")
-
-totalProdutos = 0
-limiteMax = 0
-while totalProdutos < limite:
-    print(f"\nProdutos cadastrados {totalProdutos + 1}")
-    produtosCadastrados = input("Informe os produtos para cadastrar: ")
-
-    # Dentro da sua estrutura de repetição, chame sua função “verificar_produto”, permitindo que o usuário consiga cadastrar todos os produtos e verificar se teria ou não limite sobrando para comprá-los.
-
-    totalProdutos, limiteMax = verificar_produto(limiteDeGasto)
