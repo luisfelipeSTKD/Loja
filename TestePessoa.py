4e893981f7e79d25f002ade1110833ae2067e7b7
Luis Felipe STKD <70490891+luisfelipeSTKD@users.noreply.github.com>
2022-07-18 23:14:55 -0300

    Classes

---
+++ TestePessoa.py
@@ -0,0 +1,7 @@
+from pessoa import Pessoa
+
+p1 = Pessoa('Luis', 29)
+p1.comer('maçã')
+p1.falar('Falando sobre orientação a objetos')
+p1.parar_comer()
+p1.falar('Falando sobre orientação a objetos')
