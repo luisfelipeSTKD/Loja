264e84ef8922d14232417c6abd46d0a1ee2c5ff9
luisfelipeSTKD <lfstorkidias@gmail.com>
2022-07-19 21:00:45 -0300

    Condições e Globais

--- testeObjCadastro.py
+++ testeObjCadastro.py
@@ -1,28 +1,29 @@
 class Cadastro:
 
-    anoAtual = 2022
-
-    def __init__(self, nome, funcao, salario, anoNascimento):
+    def __init__(self, nome, funcao, salario, anoNascimento, idade=0):
         self.nome = nome
         self.funcao = funcao
         self.salario = salario
         self.anoNascimento = anoNascimento
+        self.idade = idade
         # self.anoAtual = anoAtual
 
     def fezCadastro(self):
-        print(f"Seu nome é {self.nome}, profissão: {self.funcao}, salario: {self.salario:5.2f} e ano de nascimento: {self.anoNascimento}")
+        print(f"Seu nome é {self.nome}, profissão: {self.funcao}, salario: {self.salario} e ano de nascimento: {self.anoNascimento}")
         # return self.nome + ' ' + self.funcao + ' ' + self.salario + ' ' + self.anoNascimento
         return self.nome, self.funcao, self.salario, self.anoNascimento
 
-    def limiteDeGasto(self, idade):
-        print(f"Seu limite de gasto é de R${self.limiteDeGasto:5.2f} e sua idade é {self.idade}")
+    def limiteDeGasto(self):
+        print(f"Seu limite de gasto é de R${self.limiteDeGasto} e sua idade é {self.idade}")
         return self.limiteDeGasto, self.idade
-
-            
 
     @classmethod
     def fazerCadastro(self):
-        while 1:
+
+        global salario
+        global anoNascimento
+
+        while True:
             try:
                 nome = input("Insira seu nome: ")
                 funcao = input("Insura sua profissão: ")
@@ -35,10 +36,19 @@ class Cadastro:
                 break
 
     @classmethod
-    def limiteGasto(self, anoAtual, anoNascimento, salario):
-        idade = anoAtual - anoNascimento
-        while 2:
+    def obterLimiteGasto(self):
+
+        global anoAtual
+
+        while True:
             try:
+                anoAtual = 2022
+                idade = anoAtual - anoNascimento
+
+                if idade < 18:
+                    print("Pessoa menor de idade")
+                    break
+
                 limiteDeGasto = (salario * (idade / 1000)) + 1000
                 return idade, limiteDeGasto
             except:
